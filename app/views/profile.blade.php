a50eb325dab528fad223fd16c54a643f22087aaa
Paula Jacobs <paulajacobso@gmail.com>
2014-08-06 18:14:40 -0400

    added environment

--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -14,34 +14,63 @@ Welcome to HERoes In STEM
 </div>
 <div class="row">
 	@if($userid == Auth::user()->id)
-		<h3>About You </h3>
-		<p class="description"> {{$thisuser->about}} </p>
-		<h5>You have {{$likedby}} admirers </h5>
+		
+		<div class="panel panel-default">
+  			<div class="panel-heading">
+    			<h3 class="panel-title">About You</h3>
+  			</div>
+  			<div class="panel-body">
+    			<p class="description"> {{$thisuser->about}} </p>
+    			<h5>You have {{$likedby}} admirers </h5>
+  			</div>
+		</div>
 
-		<br/><br/>
 
-		<h3>Interests </h3>	
-		@foreach(Auth::user()->tags as $tag)
-			<h5>{{ $tag->name }} </h5>
-		@endforeach
+		<div class="panel panel-default">
+  			<div class="panel-heading">
+    			<h3 class="panel-title">Interests</h3>
+  			</div>
+  			<div class="panel-body">
+    			@foreach(Auth::user()->tags as $tag)
+				<h5>{{ $tag->name }} </h5>
+				@endforeach
+  			</div>
+		</div>
 
-		<h3>Personal Heroes </h3>	
+		<div class="panel panel-default">
+  			<div class="panel-heading">
+    			<h3 class="panel-title">Personal Heroes</h3>
+  			</div>
+  			<div class="panel-body">
+    			@foreach(Auth::user()->heroes as $hero)
+				<h5>{{ $hero->name }} </h5>
+				@endforeach
+  			</div>
+		</div>
 
-		@foreach(Auth::user()->heroes as $hero)
-			<h5>{{ $hero->name }} </h5>
-		@endforeach
+		<div class="panel panel-default teal">
+  			<div class="panel-heading ">
+    			<h3 class="panel-title">People You Admire</h3>
+  			</div>
+  			<div class="panel-body">
+    			@foreach($likes as $liked)
+				<h5> {{ User::find($liked->liked_id)->username }} </h5>
+				@endforeach	
+  			</div>
+		</div>
 
 		
-		
-		<h3>People You Admire </h3> 
-		@foreach($likes as $liked)
-			<h5> {{ User::find($liked->liked_id)->username }} </h5>
-		@endforeach	
 	@else
 		<h3>About {{$thisuser->username}} </h3>
 		<p class="description"> {{$thisuser->about}} </p>
 		<h5>{{$thisuser->username}} has {{$likedby}} admirers </h5>
 		<br/><br/>
+
+		<h3>Interests </h3>	
+		@foreach($thisuser->tags as $tag)
+			<h5>{{ $tag->name }} </h5>
+		@endforeach
+
 		<h3>Personal Heroes </h3>
 		@foreach($thisuser->heroes as $hero)
 			<h5>{{ $hero->name }} </h5>
