5516dfe930b2967980c15c0034a74b8967803e96
Paula Jacobs <paulajacobso@gmail.com>
2014-08-06 16:33:36 -0400

    added debug route for production

--- app/routes.php
+++ app/routes.php
@@ -394,6 +394,51 @@ Route::get('/seed', function() {
 
 });
 
+Route::get('/debug', function() {
+
+    echo '<pre>';
+
+    echo '<h1>environment.php</h1>';
+    $path   = base_path().'/environment.php';
+
+    try {
+        $contents = 'Contents: '.File::getRequire($path);
+        $exists = 'Yes';
+    }
+    catch (Exception $e) {
+        $exists = 'No. Defaulting to `production`';
+        $contents = '';
+    }
+
+    echo "Checking for: ".$path.'<br>';
+    echo 'Exists: '.$exists.'<br>';
+    echo $contents;
+    echo '<br>';
+
+    echo '<h1>Environment</h1>';
+    echo App::environment().'</h1>';
+
+    echo '<h1>Debugging?</h1>';
+    if(Config::get('app.debug')) echo "Yes"; else echo "No";
+
+    echo '<h1>Database Config</h1>';
+    print_r(Config::get('database.connections.mysql'));
+
+    echo '<h1>Test Database Connection</h1>';
+    try {
+        $results = DB::select('SHOW DATABASES;');
+        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
+        echo "<br><br>Your Databases:<br><br>";
+        print_r($results);
+    } 
+    catch (Exception $e) {
+        echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
+    }
+
+    echo '</pre>';
+
+});
+
 
 
 
